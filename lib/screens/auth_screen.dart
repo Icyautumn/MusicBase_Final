5b07a25b672f903bedac18976a79f002c26dbec7
Icyautumn <victorchua865@gmail.com>
2022-07-21 10:13:09 +0800

    chat app working

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,95 @@
+import 'package:chat_application/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  GoogleSignIn googleSignIn = GoogleSignIn();
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future signInFunction() async {
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    if (googleUser == null) {
+      return;
+    }
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+    DocumentSnapshot userExist =
+        await firestore.collection('users').doc(userCredential.user!.uid).get();
+
+    if (userExist.exists) {
+      print("User Already Exists in Database");
+    } else {
+      await firestore.collection('users').doc(userCredential.user!.uid).set({
+        'email': userCredential.user!.email,
+        'name': userCredential.user!.displayName,
+        'image': userCredential.user!.photoURL,
+        'uid': userCredential.user!.uid,
+        'date': DateTime.now(),
+      });
+    }
+
+    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>MyApp()), (route) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://cdn.iconscout.com/icon/free/png-256/chat-2639493-2187526.png"))),
+          ),
+        ),
+        Text(
+          "Flutter Chat App",
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: ElevatedButton(
+            onPressed: () async {
+              await signInFunction();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  'https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png',
+                  height: 36,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Sign in With Google",
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
+            ),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black),
+                padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(vertical: 12))),
+          ),
+        )
+      ]),
+    ));
+  }
+}
